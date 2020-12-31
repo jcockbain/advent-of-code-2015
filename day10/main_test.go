78c2fb52747fde97ccf588c4d255f701df324cd7
James <jcockbain96@gmail.com>
2020-12-31 17:55:18 +0000

    add day 10

---
+++ day10/main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPart1(t *testing.T) {
+	assert.Equal(t, 6, Part1("1", 5))
+	// assert.Equal(t, 1371, Part1("input.txt"))
+}
+// }
+// func TestPart2(t *testing.T) {
+// 	assert.Equal(t, 19, Part2("test1.txt"))
+// 	assert.Equal(t, 2117, Part2("input.txt"))
+// }
+
