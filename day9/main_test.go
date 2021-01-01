42510f42795dbc7b0709f338c6ca28a5e1c74d2d
James <jcockbain96@gmail.com>
2021-01-01 18:35:43 +0000

    remove part 2 test for day 9

--- day9/main_test.go
+++ day9/main_test.go
@@ -10,8 +10,3 @@ func TestPart1(t *testing.T) {
 	assert.Equal(t, 605, Part1("test1.txt"))
 	assert.Equal(t, 117, Part1("input.txt"))
 }
-
-func TestPart2(t *testing.T) {
-	assert.Equal(t, 982, Part2("test1.txt"))
-	assert.Equal(t, 909, Part2("input.txt"))
-}
