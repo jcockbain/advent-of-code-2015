db7beb35380d30dc701fcc8776391f074e37e841
James <jcockbain96@gmail.com>
2021-01-01 18:46:22 +0000

    actual test fix

--- day8/main_test.go
+++ day8/main_test.go
@@ -7,12 +7,10 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 605, Part1("test1.txt"))
+	assert.Equal(t, 12, Part1("test1.txt"))
+	assert.Equal(t, 1371, Part1("input.txt"))
 }
-
-// assert.Equal(t, 1371, Part1("input.txt"))
-// }
-// func TestPart2(t *testing.T) {
-// 	assert.Equal(t, 19, Part2("test1.txt"))
-// 	assert.Equal(t, 2117, Part2("input.txt"))
-// }
+func TestPart2(t *testing.T) {
+	assert.Equal(t, 19, Part2("test1.txt"))
+	assert.Equal(t, 2117, Part2("input.txt"))
+}
